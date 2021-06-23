2570b62339bac698489016f12e151b7b1d04a869
“Margaux” <margauxcoppipro@gmail.com>
2021-06-23 10:30:42 +0200

    Verion 1

--- script.js
+++ script.js
@@ -1,42 +1,36 @@
-const menuBtn = document.querySelector('.menu-btn');
-let menuOpen = false;
-menuBtn.addEventListener('click', () => {
-    if(!menuOpen) {
-        menuBtn.classList.add('open');
-        menuOpen = true;
-    } else {
-        menuBtn.classList.remove('open');
-        menuOpen = false;
-    }
-});
 
-const menuBtnBurger = document.querySelector('.menu-btn__burger');
+var skills = document.querySelector('#bar');
 
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function addanim (){
+  var bar = document.querySelectorAll('span');
+  bar.forEach(b => b.classList.add('anim'));
+}
+function onscroll(){
+  if(window.scrollY > bar.offsetTop-window.innerHeight){
+    addanim ();
+    document.removeEventListener('scroll', onscroll);
+  };
 }
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+document.addEventListener('scroll', onscroll);
 
-function showSlides(n) {
+
+showSlides(0, true);
+
+function showSlides(index, reprogram) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
+    slides[i].style.display = "none";
   }
+
   for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
+    dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+  slides[index].style.display = "block";
+  dots[index].className += " active";
+  index++;
+  if (index >= slides.length) {
+    index = 0;
+  }}
